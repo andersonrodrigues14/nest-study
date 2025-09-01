f33c68924b8ba5f779ef744666402210a19aad90
Anderson Almeida Rodrigues <anderaro@bancomoneo.com.br>
2025-09-01 15:19:22 -0300

    first commit

---
+++ conceitos_nest/src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
+
+@Module({
+  imports: [ConceitosAutomaticoModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
